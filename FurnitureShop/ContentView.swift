0fda03410925695f01b190b2bdb3f7925ff049f7
Akın Aksoy <akinaksoytr@gmail.com>
2023-05-27 22:55:09 +0300

    Initial Commit

---
+++ FurnitureShop/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FurnitureShop
+//
+//  Created by Akın Aksoy on 27.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FurnitureShop/FurnitureShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureShopApp.swift
+//  FurnitureShop
+//
+//  Created by Akın Aksoy on 27.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
